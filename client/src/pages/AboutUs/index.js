7e33178461e6e0693b1b17d4ecd90b111d865a5b
Giselle556 <giselleborbolla@yahoo.com>
2021-11-02 13:52:58 -0700

    about us pictures

--- client/src/pages/AboutUs/index.js
+++ client/src/pages/AboutUs/index.js
@@ -6,16 +6,16 @@ export default function AboutUs() {
     return (
       <div>
         <div className="about-section">
-          <p>We are a group of 3 full stack students consisting of Sophie, Keith, and Giselle</p>
-          <p>Sophie is planning to </p>
+          <p>Group of three full stack web developer students attending UofA's bootcamp. </p>
+          
         </div>
         <h2 style={{textAlign: 'center'}}>Our Team</h2>
         <div className="row">
           <div className="column">
             <div className="card">
-              <img src="/sophie.png" alt="Sophie" width="100%" height="130" />
+              <img src="/sophie.png" alt="Sophie" style={{height: '340px', width: '100%'}} />
               <div className="container">
-                <h2>Sophies Deskin</h2>
+                <h2>Sophie Deskin</h2>
                 <p className="title">Fullstack WebDev </p>
                 <p>Hi! I'm a web developer based in Scottsdale, AZ!</p>
                
@@ -24,36 +24,34 @@ export default function AboutUs() {
           </div>
           <div className="column">
             <div className="card">
-              <img src="/keith.jpg" alt="Keith" width="100%" height="45" />
+              <img src="/keith.jpg" alt="Keith" style={{height: '340px', width: '100%'}} />
               <div className="container">
                 <h2>Keith Whitman</h2>
                 <p className="title">Fullstack WebDev</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>Web developer based in Central Phoenix,AZ.</p>
                
               </div>
             </div>
           </div>
           <div className="column">
             <div className="card">
-              <img src="/me2.jpg" alt="Giselle" width="100%" height="130" />
+              <img src="/me2.jpg" alt="Giselle" style={{height: '340px', width: '100%'}} />
               <div className="container">
                 <h2>Giselle Borbolla</h2>
                 <p className="title">Fullstack Webdev</p>
-                <p>Current Student of University of Arizona's Full Stack Web Development bootcamp.</p>
+                <p>Hey! WebDev student located out in Goodyear,AZ.</p>
                
                
               </div>
              </div>
           </div> 
         </div>
-        {/* <div className="awesome" style={{border: '1px solid red'}}>
-          <label htmlFor="name">Enter your name: </label>
-          <input type="text" id="name" />
-        </div> */}
-        {/* <p>Enter your HTML here</p> */}
       </div> 
+      
     );
-
+  
 }
+
+
   
   
